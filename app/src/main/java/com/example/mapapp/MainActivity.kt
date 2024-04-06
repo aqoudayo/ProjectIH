65db9a204c0b5cda7a362cbf2d203a73499f80a9
aqoudayo <hiyeonu0520@gmail.com>
2024-04-07 02:46:28 +0900

    [Inital] Final Test Commit

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        //Test
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
